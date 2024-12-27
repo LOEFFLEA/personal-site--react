3f629d94fbdcce0566b5c5a11dce6807d3fc623f
Abigail Loeffler <LOEFFLEA@my.erau.edu>
2024-12-27 15:00:38 -0500

    Initial Build

---
+++ my-site/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Site</h1>
+      <div id="page-body">
+        Welcome to my Site!
+      </div>
+    </div>
+  );
+}
+
+export default App;
